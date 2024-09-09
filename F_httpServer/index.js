7f13b296cf8b18023586e0cf4d62df91e5d79d96
Ayush <ayushsaini1910@gmail.com>
2024-09-09 21:04:08 +0530

    req.url

--- F_httpServer/index.js
+++ F_httpServer/index.js
@@ -1,7 +1,21 @@
 const http = require('http')
 
 const server = http.createServer((req, res) => {
-    res.end('Hello from the other sides');
+    // console.log(req.url)
+    if(req.url === '/'){
+        res.end("Hello Ayush")
+    }else if(req.url === '/about'){
+        res.end("hello Ayush it is the about page")
+    }else if(req.url === '/contact'){
+        res.write("hello ayush it is the contact page")
+        res.end();
+    }else{
+        // for network status
+        // you can pass any information to the network
+        res.writeHead(404, {'content-type': "text/html", "name" : "ayush"})
+        res.end(`<h1 style="color:red">404 error pages. Page does not exist</h1>`) 
+    }
+    // res.end('Hello from the other sides , and it is second time');
 })
 
 server.listen(3000, "127.0.0.1", () => {
