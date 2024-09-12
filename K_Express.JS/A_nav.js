3cc18976c5782e6100872508d82f6ec6cda2a261
Ayush <ayushsaini1910@gmail.com>
2024-09-12 22:02:05 +0530

    nav

--- K_Express.JS/A_nav.js
+++ K_Express.JS/A_nav.js
@@ -1,2 +1,20 @@
 const express = require('express')
 
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send("Welcome to my home page")
+})
+
+app.get('/about', (req, res) => {
+    res.send("Welcome to my about page")
+})
+
+app.get('/contact', (req, res) => {
+    res.status(200).send("Welcome to my contact page")
+})
+
+app.listen(port, () => {
+    console.log(`listening to the port no. ${port}`)
+})
