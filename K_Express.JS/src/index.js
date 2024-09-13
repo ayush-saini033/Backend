ddde71c314f3fd9be621ab4a4d3f05f8c85ca8d6
Ayush <ayushsaini1910@gmail.com>
2024-09-13 10:05:26 +0530

    views

--- K_Express.JS/src/index.js
+++ K_Express.JS/src/index.js
@@ -8,22 +8,44 @@ const app = express();
 // console.log(__dirname)
 
 const staticPath = path.join(__dirname, "../public")
+const templetPath = path.join(__dirname, "../templates")
 
+app.use(express.static(staticPath))
+// to set the view engine at hbs
+app.set('view engine', "hbs")
+app.set("views", templetPath)
+// app.set('views','../views')
 
+// set the views directory
+// app.set('views',path.join(__dirname, '../views'));
+
+// console.log('views',path.join(__dirname, '../views'))
+
+// serve the file from the public directory
+// app.use(express.static(path.join(__dirname, "../public")))
+// console.log(path.join(__dirname, "../public"))
 
 // builtin middleware
-app.use(express.static(staticPath));
+// app.use(express.static(staticPath));
+
+// templet engine route
+app.get("", (req,res) =>{
+    res.render('index', {
+        tips: "official",
+    })
+})
 
 // routing
 // app.get(route, callback)
+
+app.get('/about', (req,res) => {
+    res.render('about')
+})
 
 app.get("/",(req, res) => {
     res.send("Hello World from the express")
 })
 
-app.get('/about', (req,res) => {
-    res.send("Hello from the about page")
-})
 
 app.listen(3000, () => {
     console.log("listening the port at 3000")
